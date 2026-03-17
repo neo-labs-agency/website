51a04dcea725c0cfa1f51553b48e589c1453fd09
Your New Name <nicolejogn@gmail.com>
2026-03-17 18:24:29 +0400

    fix: layout shift

--- app/root.tsx
+++ app/root.tsx
@@ -14,18 +14,6 @@ import { PageLoadBlur } from "@/shared/ui/page-load-blur/page-load-blur";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
-  // Preconnect + fonts
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-
   // Favicons & PWA assets from /public/favicon
   {
     rel: "icon",
